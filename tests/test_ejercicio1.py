6c227e806d55c8a4d779a27fc60e6aaefbfef02d
Luz <luzmtranzillo@gmail.com>
2022-06-08 21:23:31 -0300

    Cambios en los docstring de las funciones de testeo

--- tests/test_ejercicio1.py
+++ tests/test_ejercicio1.py
@@ -7,15 +7,13 @@ import pytest
 from src.ejercicio1 import es_par
 
 """
-Describir aquí que es lo que se esta probando.
-Tengan en cuenta que el archivo tiene que llamarse igual
-que el archivo a probar agregando antes `test_`
+Test que testea la funciones es_par(numero) del ejercico1.py para casos negativos y positivos, pares e impares.
 """
 
 
 def test_es_par_positivo_true():
     """
-    Función que prueba la funcione es_par(numero) para los casos de números pares positivos.
+    Función que prueba que el tipo de entrada y salida de la funcion es_par(numero) sea correcta y que el resultado para los casos de números pares positivos sea correcto.
     """
     numero=4
     resultado=es_par(numero)
@@ -25,7 +23,7 @@ def test_es_par_positivo_true():
     
 def test_es_par_negativo_true():
     """
-    Función que prueba la funcione es_par(numero) para los casos de números pares negativos.
+    Función que prueba que el tipo de entrada y salida de la funcion es_par(numero) sea correcta y que el resultado para los casos de números pares negativos sea correcto.
     """
     numero=-4
     resultado=es_par(numero)
@@ -35,7 +33,7 @@ def test_es_par_negativo_true():
     
 def test_es_par_positivo_false():
     """
-    Función que prueba la funcione es_par(numero) para los casos de números impares positivos.
+    Función que prueba que el tipo de entrada y salida de la funcion es_par(numero) sea correcta y que el resultado para los casos de números impares positivos sea correcto.
     """
     numero=3
     resultado=es_par(numero)
@@ -45,7 +43,7 @@ def test_es_par_positivo_false():
     
 def test_es_par_negativo_false():
     """
-    Función que prueba la funcione es_par(numero) para los casos de números impares negativos.
+    Función que prueba que el tipo de entrada y salida de la funcion es_par(numero) sea correcta y que el resultado para los casos de números impares negativos sea correcto.
     """
     numero=-3
     resultado=es_par(numero)
