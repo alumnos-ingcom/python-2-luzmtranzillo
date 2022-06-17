6d210a017e466bab62de39e313f3c2e300033776
Luz <luzmtranzillo@gmail.com>
2022-06-17 20:34:23 -0300

    Adicion de docstrings

--- src/ejercicio3.py
+++ src/ejercicio3.py
@@ -9,6 +9,9 @@ Desarrollar una función que indique el grado de superposición entre dos listas
 Siendo 0 sin superposición y uno para cada elemento superpuesto.
 """
 def superposicion(lista1, lista2):
+    """
+    Funcion que recibe dos listas de caracteres y retorna la cantidad de valores repetidos.
+    """
     lista1_set=set(lista1)
     lista2_set=set(lista2)
     listas=lista1_set and lista2_set
@@ -25,11 +28,10 @@ def superposicion(lista1, lista2):
     
 def principal():
     """
-    
+    Funcion que pide el ingreso de dos cadenas de caracteres en forma de lista e imprime la cantidad de elementos superpuestos en la comparacion de ambas listas.
     """
     lista1=list(input('Ingrese una cadena de caracteres: '))
     lista2=list(input('Ingrese otra cadena de caracteres: '))
-    print(lista1)
     superpuestos=superposicion(lista1, lista2)
     print (f'El grado de superposición de caracteres es de: {superpuestos} elementos.')
     
