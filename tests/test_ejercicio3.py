93ec065ebf37c16f4905a3341671a0ca47b7c0fc
Luz <luzmtranzillo@gmail.com>
2022-06-18 23:48:06 -0300

    Modificaciones en los casos de prueba de acuerdo a las moficiaciones en el ejercicio principal

--- tests/test_ejercicio3.py
+++ tests/test_ejercicio3.py
@@ -16,10 +16,10 @@ def test_superposicion():
     """
     Test que testea que las entradas y la salida de la funcion superposicion sean del tipo correcto. Además de que la salida de un resultado correcto.
     """
-    lista1="Hola mundo"
-    lista2="Hola"
-    grado=superposicion(lista1, lista2)
-    assert isinstance (lista1, str), 'La entrada debe ser de tipo string.'
-    assert isinstance (lista2, str), 'La entrada debe ser de tipo string.'
-    assert isinstance (grado, int), 'El grado de superposicion debe ser un valor de tipo entero'
-    assert grado==4, 'El grado de superposicion no es correcto.'
+    lista1=['H','o','l','a',' ','m','u','n','d','o']
+    lista2=['H','o','l','a']
+    grado_superpuestos=superposicion(lista1, lista2)
+    assert isinstance (lista1, list), 'La entrada debe ser de tipo lista.'
+    assert isinstance (lista2, list), 'La entrada debe ser de tipo lista.'
+    assert isinstance (grado_superpuestos, int), 'El grado de superposicion debe ser un valor de tipo entero'
+    assert grado_superpuestos==4, 'El grado de superposicion no es correcto.'
